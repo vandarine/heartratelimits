f19939537f489893f9d86e31d56a2f01675e9366
Schmidt Vanessa <vs095@hdm-stuttgart.de>
2022-09-09 13:49:18 +0300

    save exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [result, setResult] = useState('');
+
+  function calculate (){
+    const upper = (220 - age) * 0.85;
+    const lower = (220 - age) * 0.65;
+    setResult(lower + "-" + upper)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Heart rate limits calculator</h3>
+      <form>
+        <div>
+          <label>Age </label> <br />
+          <input type="number" value={age} onChange={e => setAge(e.target.value)}/>
+        </div>
+        <div>
+          <label>Heart rate limits </label> <br />
+          <input type="string" value={result} />
+        </div>
+        <button type="button" onClick={calculate}>Calculate</button>
+      </form>
     </div>
   );
 }
